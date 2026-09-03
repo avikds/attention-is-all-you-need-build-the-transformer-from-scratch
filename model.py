aa55e1be436051179456da90a2538def707f507f
Avik Das <avikd957@gmail.com>
2026-09-03 06:44:58 +0530

    Step 79: mark_finished_beams

--- model.py
+++ model.py
@@ -1297,8 +1297,11 @@ def append_tokens_to_beam_sequences(
         dim=1,
     )
 
-# Step 79 - mark_finished_beams (not yet solved)
-# TODO: implement
+# Step 79 - mark_finished_beams
+def mark_finished_beams(token_ids, finished_flags, end_token_id):
+    # A beam is finished if it was already finished or if it just
+    # generated the end-of-sequence token.
+    return finished_flags | (token_ids == end_token_id)
 
 # Step 80 - select_best_finished_beam (not yet solved)
 # TODO: implement
