b7a8c70927b8be4dfb3048fa966051ed3f9e2d44
Avik Das <avikd957@gmail.com>
2026-09-03 06:38:14 +0530

    Step 67: apply_adam_bias_correction

--- model.py
+++ model.py
@@ -1000,8 +1000,13 @@ def update_adam_second_moment(v_prev, grad, beta2):
         beta2 * v_prev + (1.0 - beta2) * (grad ** 2)
     ).detach()
 
-# Step 67 - apply_adam_bias_correction (not yet solved)
-# TODO: implement
+# Step 67 - apply_adam_bias_correction
+def apply_adam_bias_correction(m_t, v_t, beta1, beta2, step):
+    """Return bias-corrected (m_hat, v_hat) for Adam at the given step."""
+    m_hat = m_t / (1.0 - beta1 ** step)
+    v_hat = v_t / (1.0 - beta2 ** step)
+
+    return m_hat, v_hat
 
 # Step 69 - apply_adam_step_to_all_parameters (not yet solved)
 # TODO: implement
