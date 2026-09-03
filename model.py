5a0465ce9cae1686db57fc4bc5af590fd0021fd9
Avik Das <avikd957@gmail.com>
2026-09-03 06:37:31 +0530

    Step 65: update_adam_first_moment

--- model.py
+++ model.py
@@ -985,8 +985,11 @@ def initialize_adam_optimizer_state(parameter_list):
         "t": 0,
     }
 
-# Step 65 - update_adam_first_moment (not yet solved)
-# TODO: implement
+# Step 65 - update_adam_first_moment
+def update_adam_first_moment(m_prev, grad, beta1):
+    """Return m_t = beta1 * m_prev + (1 - beta1) * grad."""
+    # Detach so the optimizer state does not track gradients.
+    return (beta1 * m_prev + (1.0 - beta1) * grad).detach()
 
 # Step 66 - update_adam_second_moment (not yet solved)
 # TODO: implement
